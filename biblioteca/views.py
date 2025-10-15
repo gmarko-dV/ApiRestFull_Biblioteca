a19a8e3340d3468de56313434d18d009434edbc9
gmarko-dV <gian.pasco@tecsup.com>
2025-10-14 21:01:51 -0500

    Implementacion de los ViewSet

--- biblioteca/views.py
+++ biblioteca/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Autor,Categoria,Libro
+from .serializers import AutorSerializer,CategoriaSerializer,LibroSerializer
 
-# Create your views here.
+class AutorViewSet(viewsets.ModelViewSet):
+    queryset = Autor.objects.all()
+    serializer_class = AutorSerializer
+    
+class CategoriaViewSet(viewsets.ModelViewSet):
+    queryset = Categoria.object.all()
+    serializer_class = CategoriaSerializer
+
+class LibroViewSet(viewsets.ModelViewSet):
+    queryset = Libro.objects.all()
+    serializer_class = LibroSerializer        
