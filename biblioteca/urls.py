efc4b9564652659f34918cc2f339329aa0cf217e
gmarko-dV <gian.pasco@tecsup.com>
2025-10-14 21:25:57 -0500

    Subiendo urls.py

---
+++ biblioteca/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path,include
+from rest_framework.routers import DefaultRouter
+from .views import AutorViewSet,CategoriaViewSet,LibroViewSet
+
+router = DefaultRouter()
+router.register(r'autores',AutorViewSet)
+router.register(r'categorias',CategoriaViewSet)
+router.register(r'libros',LibroViewSet)
+
+urlpatterns = [
+    path('',include(router.urls)),
+]
+
+
